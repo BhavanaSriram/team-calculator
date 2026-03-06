e7a801905ece0627033510ae0dc99c54c3bd7a7c
Sriram Bhavana <bhavana@monocept.com>
2026-03-06 14:50:39 +0530

    Initial project setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Addition add = new Addition();
+        Subtraction sub = new Subtraction();
+        Multiplication mul = new Multiplication();
+
+        Console.WriteLine("Addition: " + add.DoAddition(10, 5));
+        Console.WriteLine("Subtraction: " + sub.DoSubtraction(10, 5));
+        Console.WriteLine("Multiplication: " + mul.DoMultiplication(10, 5));
+    }
+}
